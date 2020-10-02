ab419dc9379b285ca6ef2030c065798a6a2070cc
k.struzhkin <k.struzhkin@MacBook-Pro-rbkadmin.local>
2020-10-02 11:36:41 +0300

    Add create page

---
+++ src/app/templates/create-template/create-template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-template',
+  templateUrl: './create-template.component.html',
+  styleUrls: ['./create-template.component.scss']
+})
+export class CreateTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
