1109556cdbb82cb226aa247871619313d23abc30
k.struzhkin <k.struzhkin@MacBook-Pro-rbkadmin.local>
2020-10-12 23:31:42 +0300

    Add groups filter page

--- src/app/groups/groups.component.ts
+++ src/app/groups/groups.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ErrorHandlerService } from '../shared/services/utils/error-handler.service';
+import { TemplatesService } from '../templates/templates.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfigService } from '../core/config.service';
+import { SortOrder } from '../shared/constants/sort-order';
+import { OperationType } from '../shared/constants/operation-type';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Sort } from '@angular/material/sort';
+import { RemoveTemplateDialogComponent } from '../templates/remove-template-dialog/remove-template-dialog.component';
 
 @Component({
     selector: 'app-groups',
@@ -6,7 +17,65 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./groups.component.scss'],
 })
 export class GroupsComponent implements OnInit {
-    constructor() {}
+    private SIZE: number;
 
-    ngOnInit(): void {}
+    isLoadMore = false;
+    isLoading = false;
+    displayedColumns: string[] = ['id', 'text', 'edit'];
+    groups = [];
+    operationTypes = [];
+    operationType;
+    searchField;
+    sortType = SortOrder.DESC;
+
+    constructor(
+        private router: Router,
+        private errorHandlerService: ErrorHandlerService,
+        private templateService: TemplatesService,
+        private snackBar: MatSnackBar,
+        public dialog: MatDialog,
+        configService: ConfigService
+    ) {
+        this.SIZE = configService.config.pageSize;
+    }
+
+    ngOnInit(): void {
+        this.operationTypes = Object.keys(OperationType).filter((k) => typeof OperationType[k as any] === 'number');
+        this.operationType = this.operationTypes[0];
+    }
+
+    search(): void {
+        this.isLoading = true;
+    }
+
+    sortData(sort: Sort): void {
+        this.sortType = sort.direction === 'asc' ? SortOrder.ASC : SortOrder.DESC;
+        this.search();
+    }
+
+    changeSearch(newValue): void {
+        this.searchField = newValue;
+        this.search();
+    }
+
+    navigateToNew(): void {
+        this.router.navigate(['/groups/new'], { fragment: this.operationType });
+    }
+
+    navigateToEdit(id): void {
+        this.router.navigate([`/groups/${id}`], { fragment: this.operationType });
+    }
+
+    loadMore(): void {}
+
+    openDialog(removeTemplate): void {
+        const dialogRef = this.dialog.open(RemoveTemplateDialogComponent, {
+            width: '350px',
+            data: { template: removeTemplate, operationType: this.operationType },
+        });
+
+        dialogRef.afterClosed().subscribe((result) => {
+            console.log('The dialog was closed');
+        });
+    }
 }
--- src/app/groups/groups.module.ts
+++ src/app/groups/groups.module.ts
@@ -1,11 +1,56 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { GroupsRoutingModule } from './groups-routing.module';
 import { GroupsComponent } from './groups.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CoreModule } from '../core/core.module';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatSliderModule } from '@angular/material/slider';
+import { AppRoutingModule } from '../app-routing.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { CreateTemplateModule } from '../templates/create-template/create-template.module';
+import { EditTemplateModule } from '../templates/edit-template/edit-template.module';
+import { ReferencesModule } from '../references/references.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { TemplatesService } from '../templates/templates.service';
+import { ConfigService } from '../core/config.service';
+import { CommonModule } from '@angular/common';
+import { TemplatesRoutingModule } from '../templates/templates-routing.module';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { GroupsRoutingModule } from './groups-routing.module';
 
 @NgModule({
     declarations: [GroupsComponent],
-    imports: [CommonModule, GroupsRoutingModule, MatToolbarModule],
+    imports: [
+        CommonModule,
+        GroupsRoutingModule,
+        MatTableModule,
+        MatCardModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSelectModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatToolbarModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatSnackBarModule,
+        MatDialogModule,
+        FormsModule,
+    ],
+    providers: [TemplatesService, ConfigService],
 })
 export class GroupsModule {}
