ef330641420ad666c94bc44a7129c7abd39ed663
k.struzhkin <koztyastruga@gmail.com>
2020-11-25 18:49:10 +0300

    Refactor

--- src/app/shared/components/navigate-menu/navigate-menu.component.ts
+++ src/app/shared/components/navigate-menu/navigate-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavItem } from '../../model/nav-item';
+import { MenuModel } from '../../model/menu-model';
 
 @Component({
     selector: 'app-navigate-menu',
@@ -7,77 +8,5 @@ import { NavItem } from '../../model/nav-item';
     styleUrls: ['./navigate-menu.component.scss'],
 })
 export class NavigateMenuComponent {
-    navItems: NavItem[] = [
-        {
-            displayName: 'Templates',
-            iconName: 'business',
-            roles: ['fraud-officer', 'fraud-monitoring', 'fraud-support'],
-            children: [
-                {
-                    displayName: 'List',
-                    route: 'templates',
-                    roles: ['fraud-officer', 'fraud-monitoring', 'fraud-support'],
-                },
-                {
-                    displayName: 'References',
-                    route: 'references',
-                    roles: ['fraud-officer', 'fraud-monitoring', 'fraud-support'],
-                },
-            ],
-        },
-        {
-            displayName: 'Groups',
-            iconName: 'group_work',
-            roles: ['fraud-officer', 'fraud-monitoring', 'fraud-support'],
-            children: [
-                {
-                    displayName: 'List',
-                    route: 'groups',
-                    roles: ['fraud-officer', 'fraud-monitoring', 'fraud-support'],
-                },
-                {
-                    displayName: 'References',
-                    route: 'groups-reference',
-                    roles: ['fraud-officer', 'fraud-monitoring', 'fraud-support'],
-                },
-            ],
-        },
-        {
-            displayName: 'Lists',
-            iconName: 'list_alt',
-            roles: ['fraud-monitoring'],
-            children: [
-                {
-                    displayName: 'White',
-                    route: 'lists/white',
-                    iconName: 'panorama_fish_eye',
-                    roles: ['fraud-monitoring'],
-                },
-                {
-                    displayName: 'Black',
-                    route: 'lists/black',
-                    iconName: 'lens',
-                    roles: ['fraud-monitoring'],
-                },
-                {
-                    displayName: 'Grey',
-                    route: 'lists/grey',
-                    iconName: 'tonality',
-                    roles: ['fraud-monitoring'],
-                },
-            ],
-        },
-        {
-            displayName: 'Load fraud',
-            iconName: 'publish',
-            route: 'load/fraud',
-            roles: ['fraud-officer', 'fraud-monitoring', 'fraud-support'],
-        },
-        {
-            displayName: 'Emulation template',
-            iconName: 'accessibility_new',
-            route: 'emulation/template',
-            roles: ['fraud-support'],
-        },
-    ];
+    navItems: NavItem[] = MenuModel.NAV_ITEMS;
 }
