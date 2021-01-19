498ee35a762a0db989fe60f8805556d0f887e662
Aleksandra Usacheva <lazercore@me.com>
2021-01-19 11:46:55 +0300

    Rule wrap

---
+++ src/app/sections/emulation/template/template-to-rules.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'templateToRules',
+})
+export class TemplateToRulesPipe implements PipeTransform {
+    transform(template: string): string[] {
+        const rules = template.split('rule:');
+        rules.splice(0, 1);
+        return rules.map((r) => `Rule: ${r.split(',').join(', ').split(':').join(': ')}`);
+    }
+}
