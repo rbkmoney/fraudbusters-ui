8072437d76365dd1dcf85ae8a21bcc89cb5092c0
Aleksandra Usacheva <lazercore@me.com>
2021-02-16 09:29:57 +0300

    Table columns fix

--- src/app/sections/references/components/p2p-references-table/p2p-references-table.component.ts
+++ src/app/sections/references/components/p2p-references-table/p2p-references-table.component.ts
@@ -2,7 +2,6 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from 
 import { Sort } from '@angular/material/sort';
 
 import { P2pReferenceModel } from '../../../../api/fb-management/swagger-codegen/model/p2pReferenceModel';
-import { PaymentReferenceModel } from '../../../../api/fb-management/swagger-codegen/model/paymentReferenceModel';
 import { SortOrder } from '../../../../shared/constants/sort-order';
 import { Action, ActionType } from '../../action';
 
@@ -32,15 +31,15 @@ export class P2pReferencesTableComponent {
         }
     }
 
-    removeReference(reference: P2pReferenceModel | PaymentReferenceModel): void {
+    removeReference(reference: P2pReferenceModel): void {
         this.action.emit({ type: ActionType.removeReference, reference });
     }
 
-    editReference(reference: P2pReferenceModel | PaymentReferenceModel) {
+    editReference(reference: P2pReferenceModel) {
         this.action.emit({ type: ActionType.editReference, reference });
     }
 
-    goToTempalte(reference: P2pReferenceModel | PaymentReferenceModel) {
+    goToTempalte(reference: P2pReferenceModel) {
         this.action.emit({ type: ActionType.goToTemplate, reference });
     }
 }
--- src/app/sections/references/components/payment-references-table/payment-references-table.component.ts
+++ src/app/sections/references/components/payment-references-table/payment-references-table.component.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { Sort } from '@angular/material/sort';
 
-import { P2pReferenceModel } from '../../../../api/fb-management/swagger-codegen/model/p2pReferenceModel';
 import { PaymentReferenceModel } from '../../../../api/fb-management/swagger-codegen/model/paymentReferenceModel';
 import { SortOrder } from '../../../../shared/constants/sort-order';
 import { Action, ActionType } from '../../action';
@@ -32,15 +31,15 @@ export class PaymentReferencesTableComponent {
         }
     }
 
-    removeReference(reference: P2pReferenceModel | PaymentReferenceModel): void {
+    removeReference(reference: PaymentReferenceModel): void {
         this.action.emit({ type: ActionType.removeReference, reference });
     }
 
-    editReference(reference: P2pReferenceModel | PaymentReferenceModel) {
+    editReference(reference: PaymentReferenceModel) {
         this.action.emit({ type: ActionType.editReference, reference });
     }
 
-    goToTempalte(reference: P2pReferenceModel | PaymentReferenceModel) {
+    goToTempalte(reference: PaymentReferenceModel) {
         this.action.emit({ type: ActionType.goToTemplate, reference });
     }
 }
