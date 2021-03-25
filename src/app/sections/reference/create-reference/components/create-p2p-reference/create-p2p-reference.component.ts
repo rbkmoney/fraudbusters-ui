13ebf492be60024ae79d29e374ebe42dd8ac7972
hedgehog <ezhoden@mail.ru>
2021-03-26 01:25:13 +0300

    p2p

--- src/app/sections/reference/create-reference/components/create-p2p-reference/create-p2p-reference.component.ts
+++ src/app/sections/reference/create-reference/components/create-p2p-reference/create-p2p-reference.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { OperationType } from '../../../../../shared/constants/operation-type';
+import { LAYOUT_GAP_M } from '../../../../../tokens';
 import { CreateP2pReferenceService } from '../../services/create-p2p-reference.service';
 
 @Component({
@@ -14,7 +15,11 @@ export class CreateP2pReferenceComponent {
     operationType = OperationType;
     inProgress$ = this.createPaymentReferenceService.inProgress$;
 
-    constructor(private createPaymentReferenceService: CreateP2pReferenceService, private router: Router) {
+    constructor(
+        private createPaymentReferenceService: CreateP2pReferenceService,
+        private router: Router,
+        @Inject(LAYOUT_GAP_M) public layoutGapM: string
+    ) {
         this.createPaymentReferenceService.created$.subscribe((q) => {
             this.router.navigate(['/references/p2p']);
         });
